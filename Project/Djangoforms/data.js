4229588e86fafc3f06740833176860a85dad4075
shibushrestha <shibushrestha12@gmail.com>
2023-02-05 22:54:46 +0545

    fromset practice

---
+++ Project/Djangoforms/data.js
@@ -0,0 +1,20 @@
+data = {
+    'form-TOTAL_FORMS': '10',
+    'form-INITIAL_FORMS': '0',
+
+    'form-0-name':'Shibu Shreshta',
+    'form-0-year_in_school':'JR',
+
+    'form-1-name':'Boby Shreshta',
+    'form-1-year_in_school':'GR',
+
+    'form-2-name':'Pratik Rai',
+    'form-2-year_in_school':'SR',
+
+    'form-3-name':'Suman Prajapati',
+    'form-3-year_in_school':'GR',
+}
+
+
+
+initial = [{'name':'Shibu Shrestha', 'year_in_school': 'FR',}, {'name':'Suman Prajapati', 'year_in_school': 'GR',},]
